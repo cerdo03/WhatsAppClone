74f687d463865645c4a6009cf54a2f7637f09646
suryashukla <CerDo3>
2022-05-03 12:23:55 +0530

    Google sign in

--- app/src/main/java/com/example/whatsapp/SignInActivity.kt
+++ app/src/main/java/com/example/whatsapp/SignInActivity.kt
@@ -6,9 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.example.whatsapp.Models.Users
 import com.example.whatsapp.databinding.ActivitySignInBinding
 import com.example.whatsapp.databinding.ActivitySignUpBinding
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
@@ -19,6 +28,10 @@ class SignInActivity : AppCompatActivity() {
     private lateinit var mAuth: FirebaseAuth
     lateinit var database: FirebaseDatabase
     lateinit var progressDialog: ProgressDialog
+    private val REQ_ONE_TAP = 2  // Can be any integer unique to the Activity
+    private var showOneTapUI = true
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    val Req_Code: Int = 123
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,21 +42,27 @@ class SignInActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("408210016783-35ntomd74qs7dde78461u1g9b4ftu5eh.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
 
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
-        progressDialog  = ProgressDialog(this)
+        progressDialog = ProgressDialog(this)
         progressDialog.setTitle("Login")
         progressDialog.setMessage("Validating your credentials...")
 
         binding.btnSignIn.setOnClickListener {
-            if(!binding.txtEmail.text.toString().isEmpty()
-                && !binding.txtPassword.text.toString().isEmpty()){
+            if (!binding.txtEmail.text.toString().isEmpty()
+                && !binding.txtPassword.text.toString().isEmpty()
+            ) {
                 progressDialog.show()
                 mAuth.signInWithEmailAndPassword(binding.txtEmail.text.toString(),
                     binding.txtPassword.text.toString()).addOnCompleteListener(this) { task ->
                     progressDialog.dismiss()
                     if (task.isSuccessful) {
-                        val intent = Intent(this,MainActivity::class.java)
+                        val intent = Intent(this, MainActivity::class.java)
                         startActivity(intent)
 
                     } else {
@@ -53,23 +72,68 @@ class SignInActivity : AppCompatActivity() {
                     }
                 }
 
-            }
-            else{
-                Toast.makeText(this,"Enter Credentials", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Enter Credentials", Toast.LENGTH_SHORT).show()
             }
 
 
         }
-        if(mAuth.currentUser!=null){
-            val intent = Intent(this,MainActivity::class.java)
+
+        binding.btnGoogle.setOnClickListener{
+            signInGoogle()
+        }
+        if (mAuth.currentUser != null) {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
         binding.clickSignUp.setOnClickListener {
-            val intent = Intent(this,SignUpActivity::class.java)
+            val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
 
 
     }
+
+    private fun signInGoogle() {
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, Req_Code)
+    }
+
+    // onActivityResult() function : this is where
+    // we provide the task and data for the Google Account
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == Req_Code) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleResult(task)
+        }
+    }
+    private fun handleResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
+            if (account != null) {
+                UpdateUI(account)
+            }
+        } catch (e: ApiException) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun UpdateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        mAuth.signInWithCredential(credential).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val user = mAuth.currentUser
+                val users = Users()
+                users.userId = user?.uid
+                users.userName = user?.displayName
+                users.profilepic = user?.photoUrl.toString()
+                database.reference.child("Users").child(user?.uid.toString()).setValue(users)
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+
 }
